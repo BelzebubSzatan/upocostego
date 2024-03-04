06e968343163601a70a1d3e473383148d79bbb40
KacperMatlag <matlagkacper@gmail.com>
2024-03-04 17:30:37 +0100

    Add Card special actions enum

---
+++ upocostego/Models/SpecialActions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace upocostego.Models {
+    public enum SpecialActions {
+        Normal=1,
+        AddTwo,
+        AddFour,
+        Color
+    }
+}
